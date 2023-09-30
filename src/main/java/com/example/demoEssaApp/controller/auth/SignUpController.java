e074c3143ab92a0f3c3405c92ff589f233d8a04f
ibrahimafaye <ibrahimafaye@tg-datalogos.net>
2023-09-30 00:28:16 +0000

    editing error messages

--- src/main/java/com/example/demoEssaApp/controller/auth/SignUpController.java
+++ src/main/java/com/example/demoEssaApp/controller/auth/SignUpController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 /**
  *
@@ -53,10 +54,18 @@ public class SignUpController {
         return "user/signupmessage";
     }
     /** User signup process
+     * @param model
+     * @param locale
      * @param form
+     * @param bindingResult
      * @return  */
     @PostMapping ("/signup" )
-    public String postSignup(@ModelAttribute SignupForm form ) {
+    public String postSignup(Model model , Locale locale , @ModelAttribute SignupForm form , BindingResult bindingResult ) {
+        // Input check result
+        if (bindingResult.hasErrors()) {
+            // NG: Return to the user signup screen
+            return getSignupMessage(model , locale , form );
+        }
         // Redirect to login screen
         log.info(form.toString());
         return "redirect:/login";
