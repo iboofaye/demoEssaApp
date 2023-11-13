a9b35a74387bb3c283462cd8beb2d727b8357cb1
ibrahimafaye <ibrahimafaye@tg-datalogos.net>
2023-11-13 17:02:49 +0000

    AOP around implementation

--- src/main/java/com/example/demoEssaApp/aspect/LogAspect.java
+++ src/main/java/com/example/demoEssaApp/aspect/LogAspect.java
@@ -6,7 +6,9 @@ package com.example.demoEssaApp.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -34,5 +36,30 @@ public class LogAspect {
     @After("execution(* *..*.*UserService.*(..))" )
     public void endLog(JoinPoint jp ) {
         log .info("Method end: " + jp .getSignature());
+    }
+    
+    /** Log output before and after the controller is executed
+     * @param jp
+     * @return 
+     * @throws java.lang.Throwable */
+    //@Around("bean(*Controller)")
+    //@Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    @Around ("@within(org.springframework.stereotype.Controller)" )
+    public Object startLog(ProceedingJoinPoint jp ) throws Throwable {
+        // Output start log
+        log .info("Method start: " + jp .getSignature());
+        try {
+            // Method execution
+            Object result = jp .proceed();
+            // Output end log
+            log .info("Method end: " + jp .getSignature());
+            // Return the execution result to the caller
+            return result ;
+        } catch (Exception e ) {
+            // Output error log
+            log .error("Method abend: " + jp .getSignature());
+            // Rethrow the error
+            throw e ;
+        }
     }
 }
